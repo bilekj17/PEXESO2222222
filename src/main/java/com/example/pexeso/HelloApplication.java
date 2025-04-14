87f038c13e776ca4c5149202fd258c4d364ed1c1
bilekj <bilekj@gasos-ro.cz>
2025-04-14 14:40:34 +0200

    l

--- src/main/java/com/example/pexeso/HelloApplication.java
+++ src/main/java/com/example/pexeso/HelloApplication.java
@@ -6,12 +6,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 350);
+        Scene scene = new Scene(fxmlLoader.load(), 550, 350);
+        URL css = getClass().getResource("Style.css");
+        if (css != null) {
+            scene.getStylesheets().add(css.toExternalForm());
+        }
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
